7238a93f5e9080c89c0fbc58ebb56d9248f56aee
Dorian Jolivald <dorian.jolivald@true.nl>
2024-08-22 11:42:40 +0200

    remove bad nics

--- opennebula/resource_opennebula_virtual_router_test.go
+++ opennebula/resource_opennebula_virtual_router_test.go
@@ -149,9 +149,6 @@ func TestAccVirtualRouter(t *testing.T) {
 					resource.TestCheckResourceAttr("opennebula_virtual_router_nic.nic1", "floating_only", "false"),
 					resource.TestCheckResourceAttr("opennebula_virtual_router_nic.nic2", "floating_ip", "false"),
 					resource.TestCheckResourceAttr("opennebula_virtual_router_nic.nic2", "floating_only", "false"),
-					resource.TestCheckResourceAttr("opennebula_virtual_router_nic.nic3", "floating_ip", "yes"),
-					resource.TestCheckResourceAttr("opennebula_virtual_router_nic.nic3", "floating_only", "false"),
-					resource.TestCheckResourceAttr("opennebula_virtual_router_nic.nic3", "ip", "10.0.0.2"),
 					testAccCheckVirtualRouterPermissions(&shared.Permissions{
 						OwnerU: 1,
 						OwnerM: 1,
@@ -549,12 +546,6 @@ resource "opennebula_virtual_router_nic" "nic1" {
   virtual_router_id = opennebula_virtual_router.test.id
   network_id        = opennebula_virtual_network.network1.id
 }
-
-resource "opennebula_virtual_router_nic" "nic3" {
-  ip 				= "10.0.0.1"
-  virtual_router_id = opennebula_virtual_router.test.id
-  network_id        = opennebula_virtual_network.network1.id
-}
 `
 var testAccVirtualRouterUpdateNICs = testAccVirtualRouterMachineTemplate + testAccVirtualRouterVNet + `
 
@@ -600,14 +591,6 @@ resource "opennebula_virtual_router_nic" "nic1" {
 	floating_ip       = false
 	virtual_router_id = opennebula_virtual_router.test.id
 	network_id        = opennebula_virtual_network.network1.id
-}
-
-
-resource "opennebula_virtual_router_nic" "nic3" {
-  floating_ip       = true
-  ip 				= "10.0.0.2"
-  virtual_router_id = opennebula_virtual_router.test.id
-  network_id        = opennebula_virtual_network.network1.id
 }
 `
 
